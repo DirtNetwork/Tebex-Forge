ad0728a62cfdcb25d41dc4093be8ff9a6a0ce00a
Marc B. <marcbeckhaeuser@gmail.com>
2025-06-05 18:40:25 +0200

    Commented out version checks.

--- forge-1.18.2/src/main/java/com/github/xniter/tebexio/util/VersionCheck.java
+++ forge-1.18.2/src/main/java/com/github/xniter/tebexio/util/VersionCheck.java
@@ -2,18 +2,9 @@ package com.github.xniter.tebexio.util;
 
 
 import com.github.xniter.tebexio.TebexForged;
-import com.github.xniter.tebexio.command.ForgeMessageUtil;
 import net.buycraft.plugin.data.responses.Version;
-import net.buycraft.plugin.shared.util.VersionUtil;
-import net.minecraft.Util;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import static net.buycraft.plugin.shared.util.VersionUtil.isVersionGreater;
 
 public class VersionCheck {
     private final TebexForged plugin;
@@ -29,34 +20,20 @@ public class VersionCheck {
     }
 
     public void verify() throws IOException {
-        if (pluginVersion.endsWith("-SNAPSHOT") || pluginVersion.equalsIgnoreCase("NONE")) {
-            return; // SNAPSHOT and IDE runServer versions ignore updates
-        }
-
-        lastKnownVersion = VersionUtil.getVersion(plugin.getHttpClient(), "forge", secret);
-        if (lastKnownVersion == null) {
-            return;
-        }
-
-        // Compare versions
-        String latestVersionString = lastKnownVersion.getVersion();
-        if (!latestVersionString.equals(pluginVersion)) {
-            upToDate = !isVersionGreater(pluginVersion, latestVersionString);
-            if (!upToDate) {
-                plugin.getLogger().info(ForgeMessageUtil.format("update_available", lastKnownVersion.getVersion()));
-            }
-        }
+        // skip
     }
 
-    @SubscribeEvent
-    public void onPostLogin(final PlayerEvent.PlayerLoggedInEvent event) {
-        if (event.getPlayer().hasPermissions(2) && !upToDate) {
-            plugin.getPlatform().executeAsyncLater(() ->
-                            event.getPlayer().sendMessage(new TextComponent(ForgeMessageUtil.format("update_available", lastKnownVersion.getVersion()))
-                                    .setStyle(CmdUtil.INFO_STYLE), Util.NIL_UUID),
-                    3, TimeUnit.SECONDS);
-        }
-    }
+    // we don't care
+    // @SubscribeEvent
+    // public void onPostLogin(final PlayerEvent.PlayerLoggedInEvent event) {
+    //     if (event.getPlayer().hasPermissions(2) && !upToDate) {
+    //         plugin.getPlatform().executeAsyncLater(() ->
+    //                         event.getPlayer().sendMessage(new TextComponent(ForgeMessageUtil
+    //                        .format("update_available", lastKnownVersion.getVersion()))
+    //                                 .setStyle(CmdUtil.INFO_STYLE), Util.NIL_UUID),
+    //                 3, TimeUnit.SECONDS);
+    //     }
+    // }
 
     public Version getLastKnownVersion() {
         return this.lastKnownVersion;
